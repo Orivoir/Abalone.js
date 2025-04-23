ca884a119f9dd318cc00d8d063156a4f20a6bace
orivoir <sam.gabor@hotmail.com>
2025-04-23 16:12:44 +0200

    fix(config) eslint flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,33 @@
-// eslint.config.mjs
-import eslintPluginPrettier from 'eslint-plugin-prettier'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'; // Import nécessaire pour le plugin
+
+
+import path from 'path';
+
+// Récupère le répertoire du fichier actuel
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default [
-  // Config TypeScript avec type-checking
-  ...tseslint.configs.recommendedTypeChecked, // utilise recommended + parsing avec types
   {
     languageOptions: {
+      parser: typescriptParser,
       parserOptions: {
-        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
     },
     plugins: {
       prettier: eslintPluginPrettier,
+      '@typescript-eslint': typescriptEslintPlugin,
     },
     rules: {
-      'prettier/prettier': ['error', { semi: false }],
       'no-console': 'warn',
+      'prettier/prettier': ['error', { semi: false }],
       'no-debugger': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
+    ignores: ['coverage/', 'jest.config.js', 'eslint.config.mjs'],
   },
-  eslintConfigPrettier, // désactive les règles ESLint conflictuelles avec Prettier
-]
+  eslintConfigPrettier,
+];
